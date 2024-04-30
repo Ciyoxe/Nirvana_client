a762f8ca7848685c1e765839fbc15136dca89158
ciyoxe <ciyoxe@gmail.com>
2024-04-30 13:15:06 +0700

    Set patch aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue  from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            "@components" : path.resolve(__dirname, "src/components"),
+            "@stores"     : path.resolve(__dirname, "src/stores"),
+            "@pages"      : path.resolve(__dirname, "src/pages"),
+            "@"           : path.resolve(__dirname, "src"),
+        }
+    }
 })
