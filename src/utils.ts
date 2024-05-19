c1c19f907afe02f41f8e8ff092b906d7f855173e
ciyoxe <ciyoxe@gmail.com>
2024-05-19 16:16:08 +0700

    Signals

--- src/utils.ts
+++ src/utils.ts
@@ -23,7 +23,35 @@ export async function sendRequest(method: string, url: string, data: object, han
         handler["_"](json ?? {});
 }
 
-export async function useInterval(time: number, fn: ()=> void) {
+export function useInterval(time: number, fn: ()=> void) {
     const interval = setInterval(fn, time);
     onBeforeUnmount(() => clearInterval(interval));
+}
+
+export function useSignal<T = void>(signal: Signal<T>, handler: (data: T)=> void) {
+    signal.add(handler);
+    onBeforeUnmount(() => signal.remove(handler));
+}
+
+export class Signal<T = void> {
+    private handlers = new Set<(data: T) => void>();
+    private onces    = new Array<(data: T) => void>();
+
+    add(handler: (data: T)=> void) {
+        this.handlers.add(handler);
+    }
+    remove(handler: (data: T)=> void) {
+        this.handlers.delete(handler);
+    }
+    once(handler: (data: T)=> void) {
+        this.onces.push(handler);
+    }
+    emit(data: T) {
+        for (const handler of this.handlers)
+            handler(data);
+        for (const handler of this.onces)
+            handler(data);
+
+        this.onces.length = 0;
+    } 
 }
