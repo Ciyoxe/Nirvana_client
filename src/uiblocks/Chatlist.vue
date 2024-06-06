0e021b3f9866b4154b4c87bd149ce00b4a5f6fb3
ciyoxe <ciyoxe@gmail.com>
2024-06-06 20:47:45 +0700

    Chat list now shows only personal chats

--- src/uiblocks/Chatlist.vue
+++ src/uiblocks/Chatlist.vue
@@ -46,36 +46,54 @@ const error = (text: string)=> {
 </script>
 
 <template>
-<h2>Ваши чаты:</h2>
-<VList lines="one" width="100%" height="100%" class="list flex-col">
-    <VListItem v-for="chat of chats.userChats" :key="chat.id" rounded="lg">
-        <div class="flex-row" @click="enterChat(chat.id)">
-            <div class="flex-row content">
-                <VListItemTitle class="title">
-                    {{ chat.name }}
-                    <VListItemSubtitle>{{ chat.preview }}</VListItemSubtitle>
-                </VListItemTitle>
-                <VBtn :icon="mdiDelete" @click.stop="tryDeleteChat(chat.id)"></VBtn>
+<div v-if="chats.userChats.length > 0">
+    <h2>Ваши чаты:</h2>
+    <VAlert v-if="chats.inAnonChat"
+        @click="$router.push('/anonchat')" 
+        type="info" 
+        rounded="lg"
+        title="У вас есть активный анонимный чат" 
+        style="user-select: none;"
+    >
+        Нажмите чтобы перейти
+    </VAlert>
+    <VList lines="one" width="100%" height="100%" class="list flex-col">
+        <VListItem v-for="chat of chats.userChats.filter(c => c.type === 'personal')" :key="chat.id" rounded="lg">
+            <div class="flex-row" @click="enterChat(chat.id)">
+                <div class="flex-row content">
+                    <VListItemTitle class="title">
+                        {{ chat.name }}
+                        <VListItemSubtitle>{{ chat.preview }}</VListItemSubtitle>
+                    </VListItemTitle>
+                    <VBtn :icon="mdiDelete" @click.stop="tryDeleteChat(chat.id)"></VBtn>
+                </div>
             </div>
-        </div>
-    </VListItem>
-</VList>
-<VDialog v-model="state.dialogShown" max-width="1000px">
-    <VCard>
-        <VCardTitle>Подтвердите удаление</VCardTitle>
-        <VCardText>Вы уверены, что хотите удалить чат? Это действие нельзя будет отменить.</VCardText>
-        <VCardActions>
-            <VBtn color="primary" @click="state.dialogShown = false">Отмена</VBtn>
-            <VBtn color="error"   @click="confirmDeletion()">Удалить</VBtn>
-        </VCardActions>
-    </VCard>
-</VDialog>
-<VSnackbar v-model="state.error" color="error" timeout="3000" rounded="lg">
-    {{ state.errorText }}
-</VSnackbar>
+        </VListItem>
+    </VList>
+    <VDialog v-model="state.dialogShown" max-width="1000px">
+        <VCard>
+            <VCardTitle>Подтвердите удаление</VCardTitle>
+            <VCardText>Вы уверены, что хотите удалить чат? Это действие нельзя будет отменить.</VCardText>
+            <VCardActions>
+                <VBtn color="primary" @click="state.dialogShown = false">Отмена</VBtn>
+                <VBtn color="error"   @click="confirmDeletion()">Удалить</VBtn>
+            </VCardActions>
+        </VCard>
+    </VDialog>
+    <VSnackbar v-model="state.error" color="error" timeout="3000" rounded="lg">
+        {{ state.errorText }}
+    </VSnackbar>
+</div>
+<VEmptyState v-else
+    headline = "У вас нет чатов"
+    title = "Находите людей в анонимном чате или в списке постов"
+/>
 </template>
 
 <style scoped lang="scss">
+h2 {
+    margin-bottom: 20px;
+}
 .list {
     display: flex;
     gap: 10px;
