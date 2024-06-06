07abd8cc08ac18b26e4d68ccfa45b66898ec70bc
ciyoxe <ciyoxe@gmail.com>
2024-06-06 19:55:30 +0700

    Profiles loading

--- src/pages/Profiles.vue
+++ src/pages/Profiles.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { getProfilesList } from '@/api/profiles';
 import Profilescontrol from '@components/Profilescontrol.vue';
+import { useProfilesStore } from '@stores/profiles';
+import { reactive } from 'vue';
+
+const profiles = useProfilesStore();
+const state    = reactive({
+    error: false,
+});
+
+getProfilesList({
+    200: (data)=> {
+        profiles.userProfiles = data.profiles;
+    },
+    '_': ()=> {
+        state.error = true;
+    }
+});
 </script>
 
 <template>
 <Profilescontrol/>
+<VSnackbar v-model="state.error" color="error" timeout="3000" rounded="lg">"Произошла ошибка получения списка профилей, повторите попытку позже"</VSnackbar>
 </template>
 
 <style scoped lang="scss">
